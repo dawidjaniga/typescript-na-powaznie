5f1a0c9918f49fcbd5247df5a8dad3e375355059
Dawid Janiga <dawidjaniga@gmail.com>
2020-12-23 02:22:22 +0100

    feat: invariant and bivariant lesson

--- src/contra-co-variants.ts
+++ src/contra-co-variants.ts
@@ -17,7 +17,9 @@ type Admin = {
 type processUser = (u: User) => User
 
 
-type Covariant<T> = Required<T>
+type Covariant<T> = () => T
+type Contravariant<T> = (x: T) => void
+
 
 let x1: Covariant<Admin> = {
     name: "John",
@@ -26,8 +28,8 @@ let x1: Covariant<Admin> = {
 }
 
 let y1: Covariant<User> = {
-name: "Dennis",
-age: 21
+    name: "Dennis",
+    age: 21
 }
 
 let z1: Covariant<Character> = {
@@ -36,3 +38,73 @@ let z1: Covariant<Character> = {
 
 y1 = x1 // ok
 y1 = z1 // error
+
+let x2: Contravariant<Admin> = {
+    name: "John",
+    age: 20,
+    role: 'owner'
+}
+
+let y2: Contravariant<User> = {
+    name: "Dennis",
+    age: 21
+}
+
+let z2: Contravariant<Character> = {
+    name: 'Carrol'
+}
+
+y2 = x2 // error
+y2 = z2 // ok
+
+
+
+type Bivariant<T> = {
+    bivariantHack(x: T): void
+}["bivariantHack"]
+type Invariant<T> = (x: T) => T
+
+
+
+let x3: Bivariant<Admin> = {
+    name: "John",
+    age: 20,
+    role: 'owner'
+}
+
+let y3: Bivariant<User> = {
+    name: "Dennis",
+    age: 21
+}
+
+let z3: Bivariant<Character> = {
+    name: 'Carrol'
+}
+
+y3 = x3 // ok
+y3 = z3 // ok
+
+
+let x4: Invariant<Admin> = {
+    name: "John",
+    age: 20,
+    role: 'owner'
+}
+
+let y4: Invariant<User> = {
+    name: "Dennis",
+    age: 21
+}
+
+let z4: Invariant<Character> = {
+    name: 'Carrol'
+}
+
+y4 = x4 // error
+y4 = z4 // error
+
+
+
+
+
+
